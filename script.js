0cd3b8f6c2459353286fb60831e713946f46eb2a
Yasser Jaziri <jaziri.yesser@gmail.com>
2026-04-28 15:08:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Typing effect
-const text = "AI | Data Analyst | Problem Solver";
+const text = "AI Engineer | Data Analyst | Problem Solver";
 let i = 0;
 
 function typing() {
     if (i < text.length) {
         document.getElementById("typing").innerHTML += text.charAt(i);
         i++;
-        setTimeout(typing, 50);
+        setTimeout(typing, 40);
     }
 }
 typing();
 
-// Cursor glow
 document.addEventListener("mousemove", e => {
     const cursor = document.getElementById("cursor");
     cursor.style.left = e.clientX + "px";
